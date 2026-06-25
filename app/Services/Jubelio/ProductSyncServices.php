4c378f6fcd17fd8e0a1b6fcd1da7290aca2262f5
ApregiPrataYuda <apregipratayda@gmail.com>
2026-06-25 17:25:16 +0700

    modifed

--- app/Services/Jubelio/ProductSyncServices.php
+++ app/Services/Jubelio/ProductSyncServices.php
@@ -8,7 +8,7 @@ use App\Models\SyncLog;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class ProductSyncService
+class ProductSyncServices
 {
     public function __construct(
         private readonly JubelioClient $client
